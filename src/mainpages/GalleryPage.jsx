169a7d31154f426906b6ad4b71e2a5d520245621
exodus31 <sarthak.nandre31@gmail.com>
2023-05-22 21:29:06 -0700

    dfeqa

--- src/mainpages/GalleryPage.jsx
+++ src/mainpages/GalleryPage.jsx
@@ -1,28 +1,23 @@
-import React from 'react'
-import Navbar from '../components/navbar'
-import Footer from '../components/footer';
-import Gallery from '../components/gallery';
+import React from "react";
+import Navbar from "../components/navbar";
+import Footer from "../components/footer";
+import Gallery from "../components/gallery";
 
 const GalleryPage = () => {
   return (
     <>
-   <div className="body">
-    <div>
-      <Navbar/>
-    </div>
-    <div className="head">
-     
-GALLERY
-      
+      <div className="body">
+        <div>
+          <Navbar />
+        </div>
+        <div className="head">GALLERY</div>
+        <Gallery />
+        <div>
+          <Footer />
+        </div>
       </div>
-
-    <Gallery />
-    <div>
-      <Footer/>
-    </div>
-    </div>
     </>
-  )
-}
+  );
+};
 
-export default GalleryPage
+export default GalleryPage;
