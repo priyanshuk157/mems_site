a81eea188831e3095f19ac02cfd1836bb55d5030
priyanshuk157 <mems210005035@iiti.ac.in>
2023-06-01 10:16:45 +0530

    aad

--- src/components/section.jsx
+++ src/components/section.jsx
@@ -6,12 +6,13 @@ const section = () => {
   return (
     <>
     <div className='sec'>
-       <div className='sec-cont '>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque assumenda ullam quam asperiores nostrum maxime esse, excepturi et laudantium necessitatibus similique unde iusto ipsam hic quas voluptate quisquam quaerat commodi.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam asperiores sunt expedita vel tempora explicabo aliquam aut corrupti optio, eligendi necessitatibus veniam hic vitae, officiis, ipsum dicta quia accusamus laborum?Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure magnam quae, officiis iusto architecto amet quam repellat nesciunt aut. Quo temporibus odio ullam dolore dolorum fugiat quisquam cupiditate nobis quam Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reprehenderit sit fuga temporibus ducimus omnis atque optio. Saepe facilis libero necessitatibus ipsa dolorum quisquam molestias amet, odit ut molestiae earum cupiditate? Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex neque consectetur reiciendis. Totam enim, facilis at mollitia, consectetur laborum necessitatibus deleniti earum provident alias vero! Culpa quos quasi sit amet?
-        </div>
+      
         <div className='sec-img'>
         <img src="https://iiti.ac.in/people/~ajaykk/ajaykk.jpg" alt="" />
+        </div>
+        <div className='sec-cont '>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque assumenda ullam quam asperiores nostrum maxime esse, excepturi et laudantium necessitatibus similique unde iusto ipsam hic quas voluptate quisquam quaerat commodi.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam asperiores sunt expedita vel tempora explicabo aliquam aut corrupti optio, eligendi necessitatibus veniam hic vitae, officiis, ipsum dicta quia accusamus laborum?Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure magnam quae, officiis iusto architecto amet quam repellat nesciunt aut. Quo temporibus odio ullam dolore dolorum fugiat quisquam cupiditate nobis quam Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reprehenderit sit fuga temporibus ducimus omnis atque optio. Saepe facilis libero necessitatibus ipsa dolorum quisquam molestias amet, odit ut molestiae earum cupiditate? Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ex neque consectetur reiciendis. Totam enim, facilis at mollitia, consectetur laborum necessitatibus deleniti earum provident alias vero! Culpa quos quasi sit amet?
         </div>
        
     </div>
